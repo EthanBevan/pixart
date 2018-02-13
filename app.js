a2136615a9ebab2b23d814fb9438d678f0894f28
Ethan <ethanbevan00@yahoo.com>
2018-02-13 15:35:31 +1100

    attempting third commit

--- app.js
+++ app.js
@@ -28,4 +28,59 @@ inputValue.addEventListener("keyup", function(event) {
 });
 
   
+//Commit 3 
+	// var $canvas = $('<canvas></canvas>')
+
+	// var $display = $('.display')
+
+
+	// $display.append($canvas)
+
+
+
+// <canvas id="myCanvas" width="100" height="100"></canvas>
+
+// // var newdivElem = $('<div>')
+// var newH2Elem = $('<h2>')
+// var newH2ElemPoster = $('<img>').attr('src', movie.Poster)
+// newH2Elem.text(movie.Title)
+// newH2ElemPoster.text(movie.Poster)
+// newdivElem.append(newH2Elem)
+// newdivElem.append(newH2ElemPoster)
+
+
+
+// $options_display.append(newdivElem)
+// $options_display.append(newH2ElemPoster)
+// console.log(movie.Year)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
